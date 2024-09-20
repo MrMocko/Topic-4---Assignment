8ba88007a23d52c20e7634701509a706d9c7c4db
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-20 09:39:13 -0400

    Add project files.

---
+++ Topic 4 - Assignment/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Topic_4___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Andre
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
