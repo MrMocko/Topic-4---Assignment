9acbea9b0fe122cf40eb99af409adb729b1ae3f8
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-23 09:39:49 -0400

    not done

--- Topic 4 - Assignment/Program.cs
+++ Topic 4 - Assignment/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {   //Andre
+            string  name;
+            int age;
+            double money;
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello. What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("Hi there " +  name + (" how old are you again? "));
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("So your " + age + (" eh? Thats not old at all! "));
+            
+            Console.WriteLine("How much do you make? " + name);
+            double.TryParse(Console.ReadLine(), out money);
+            Console.WriteLine(money .ToString("C") + " I hope thats per hour not per year");
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Please enter your info so i can steal your personal info. :3);
+
         }
     }
 }
