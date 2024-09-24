566a6d2d0cc1add0c48af91f52834ee7c26e9e20
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-24 09:38:50 -0400

    Almost done :)

--- Topic 4 - Assignment/Program.cs
+++ Topic 4 - Assignment/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace Topic_4___Assignment
+﻿using System.Transactions;
+
+namespace Topic_4___Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {   //Andre
-            string  name;
-            int age;
-            double money;
+            string  name, firstname, lastname, login;
+            int age, grade, id;
+            double money, average;
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello. What is your name?");
             name = Console.ReadLine();
@@ -20,8 +22,43 @@
             Console.WriteLine("Press Enter to continue");
             Console.ReadLine();
             Console.Clear();
-            Console.WriteLine("Please enter your info so i can steal your personal info. :3);
+            Console.WriteLine("Please enter your info so i can steal your personal info. :3");
+            Thread.Sleep(1000);
+            Console.WriteLine("Enter your firstname");
+            firstname = Console.ReadLine();
+            Console.WriteLine("Enter your last name");
+            lastname = Console.ReadLine();
+            Console.WriteLine("Enter your grade");
+            Int32.TryParse (Console.ReadLine(), out grade);
+            Console.WriteLine("Enter your student id");
+            Int32.TryParse (Console.ReadLine(), out id);
+            Console.WriteLine("Enter your login"); 
+            login = Console.ReadLine();
+            Console.WriteLine("Enter your average");
+            double.TryParse (Console.ReadLine(), out average);
 
+            Console.WriteLine("\tLogin : " + login);
+            Console.WriteLine("\tStudent ID : " + id);
+            Console.WriteLine("\tFirst name : " + firstname + " " +lastname);
+            Console.WriteLine("\tYour Average :\t" + average + "%");
+            Console.WriteLine("\tGrade : " + grade);
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear ();
+            Console.WriteLine("Hello what is your name?" + name);
+            Console.ReadLine();
+            Console.WriteLine("Hi " + name + "! How old are you? ");
+            Int32.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Did you know that in 5 years you will be " + (age + 5) + (" Years old?!"));
+            Thread.Sleep(1000);
+            Console.WriteLine("And 5 years ago you were " + (age - 5) + (" Years old"));
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Console.Clear ();
+            Console.WriteLine();
+            
+            
+            
         }
     }
 }
